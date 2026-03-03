eaeea29f3f6687fed0d6e1c03f190756ef336490
Anointing Tamunowunari-Tasker <anointingtasker2002@gmail.com>
2026-03-03 23:50:50 +0100

    hid supabase proj id

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
 
-export const projectId = import.meta.env.VITE_SUPABASE_PROJECT_ID || "eexvfmdgnawghtorjito";
+export const projectId = import.meta.env.VITE_SUPABASE_PROJECT_ID || "";
 export const publicAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
